9090224b6ddb4b001078714093aecd18dd027b5b
Jakaria <jamahmud2022@gmail.com>
2026-03-08 15:45:29 -0400

    add executeable

--- Homework5.php
+++ Homework5.php
@@ -1,5 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 include 'dbconfig.php';
 $db = connectDB();
+$select = $db->prepare("SELECT * FROM
+movies;");
+$select->execute();
+$info = $select->fetchAll();
+//echo "db succes";
+
 
  ?>
